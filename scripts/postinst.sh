51583f5d289a26a9e1a83de8170c35f0b2c108ff
Philipp Heckel <pheckel@datto.com>
2022-01-13 17:16:04 -0500

    Attachments dir in package

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -8,8 +8,8 @@ if [ "$1" = "configure" ] || [ "$1" -ge 1 ]; then
   if [ -d /run/systemd/system ]; then
     # Create ntfy user/group
     id ntfy >/dev/null 2>&1 || useradd --system --no-create-home ntfy
-    chown ntfy.ntfy /var/cache/ntfy
-    chmod 700 /var/cache/ntfy
+    chown ntfy.ntfy /var/cache/ntfy /var/cache/ntfy/attachments
+    chmod 700 /var/cache/ntfy /var/cache/ntfy/attachments
 
     # Hack to change permissions on cache file
     configfile="/etc/ntfy/server.yml"

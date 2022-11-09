be2e5c4c9bb261c1565f356a28a242dc1ba2cc26
Liam <liam.stojanovic@gmail.com>
2022-11-09 10:02:46 -0500

    service worker boilerplate

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
+
+serviceWorkerRegistration.register();

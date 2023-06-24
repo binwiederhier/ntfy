dd02267f9b3e314ed04948c24da172477af9d26a
nimbleghost <132819643+nimbleghost@users.noreply.github.com>
2023-06-24 20:11:10 +0200

    Disable PWA (manifest) when web push is disabled

--- server/server_middleware.go
+++ server/server_middleware.go
@@ -60,7 +60,7 @@ func (s *Server) ensureWebEnabled(next handleFunc) handleFunc {
 
 func (s *Server) ensureWebPushEnabled(next handleFunc) handleFunc {
 	return func(w http.ResponseWriter, r *http.Request, v *visitor) error {
-		if s.config.WebPushPublicKey == "" {
+		if s.config.WebRoot == "" || s.config.WebPushPublicKey == "" {
 			return errHTTPNotFound
 		}
 		return next(w, r, v)
